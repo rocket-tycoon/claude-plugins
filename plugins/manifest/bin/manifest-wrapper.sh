3de0422b052169e4d8f3072a74e9811af816db43
Alastair Dawson <283834+kreek@users.noreply.github.com>
2026-01-18 22:22:43 -0800

    Update manifest to 0.1.23

--- plugins/manifest/bin/manifest-wrapper.sh
+++ plugins/manifest/bin/manifest-wrapper.sh
@@ -11,7 +11,7 @@ fi
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 MANIFEST_BIN="$SCRIPT_DIR/manifest"
-VERSION="0.1.22"
+VERSION="0.1.23"
 
 # Detect platform
 case "$(uname -s)" in
